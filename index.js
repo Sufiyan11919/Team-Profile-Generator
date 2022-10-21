c0aa30565f8e99348ae9422ff942014003a1f94a
sufi <sufiyanshaikh11919@gmail.com>
2022-10-21 03:08:58 -0700

    done with index.js and added css to html

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const fs = require("fs");
+const inquirer = require("inquirer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
-const inquirer = require("inquirer");
+const fs = require("fs");
+// const indexPage = require("./generate")
 
-const employees = []
+const allTeam = []
 
 const addManager = () => {
 
@@ -72,7 +73,7 @@ const addManager = () => {
     .then(managerData => {
         const { name, id, email, office } = managerData;
         const manager = new Manager(name, id, email, office);
-        employees.push(manager);
+        allTeam.push(manager);
     })
 };
 
@@ -119,7 +120,7 @@ return inquirer
         type: "input",
         name: "id",
         message: "What is their employee ID?",
-        validate: nameInput => {
+        validate: (nameInput) => {
             if (nameInput) {
                 return true;
             } else {
@@ -132,7 +133,7 @@ return inquirer
         type: "input",
         name: "email",
         message: "Provide the employee's email address.",
-        validate: nameInput => {
+        validate: (nameInput) => {
             if (nameInput) {
                 return true;
             } else {
@@ -176,29 +177,111 @@ return inquirer
         default: false
     }
 ])
-.then(empData => {
-    if(empData.role === 'Intern') {
-        const { name, id, email, school } = empData;
+.then(data => {
+    if(data.role === 'Intern') {
+        const { name, id, email, school } = data;
         const intern = new Intern(name, id, email, school);
-        employees.push(intern);
+        allTeam.push(intern);
     }
     else {
-        const { name, id, email, github } = empData;
+        const { name, id, email, github } = data;
         const engineer = new Engineer(name, id, email, github);
-        employees.push(engineer); 
+        allTeam.push(engineer); 
     }
 
-    if(empData.confirmAddEmployee) {
-        return addEmployee(empData);
+    if(data.confirmAddEmployee) {
+        return addEmployee(data);
     } else {
-        return employees;
+        return allTeam;
     }
 });
 }
+
+// **************************************************************************************************
+
+const indexPage = () => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team-Profile</title>
+        <link rel="stylesheet" href="./style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        
+    </head>
+    <body>
+        <header class="header ">
+            <h1>Team<h1>
+        </header>
+
+        <div class = "container">
+            ${allTeam
+                .filter((employee) => employee instanceof Manager)
+                .map((manager) => `
+                    <div class="block">
+                        <div class="block-header">
+                            <h3>${manager.name}</h3>
+                            <h4>Manager</h4>
+                        </div>
+                        <div class="block-body">
+                            <p>ID: ${manager.id}</p>
+                            <p>Email: 
+                                <a href="mailto:${manager.email}">${manager.email}</a>
+                            </p>
+                            <p>Office Number: ${manager.office}</p>
+                        </div>
+                    </div>`)}
+
+            ${allTeam
+                .filter((employee) => employee instanceof Engineer)
+                .map((engineer) => `
+                    <div class="block">
+                            <div class="block-header">
+                                <h3>${engineer.name}</h3>
+                                <h4>Engineer</h4>
+                            </div>
+                        <div class="block-body">
+                            <p>ID: ${engineer.id}</p>
+                            <p>Email: 
+                                <a href="mailto:${engineer.email}">${engineer.email}</a>
+                            </p>
+                            <p>Github: 
+                                <a href="https://github.com/${engineer.github}" class="mt-auto"><i class="fab fa-github mr-2"></i>${engineer.github}</a>
+                            </p>
+                        </div>
+                    
+                    </div>`)}
+
+            ${allTeam
+                .filter((employee) => employee instanceof Intern)
+                .map((intern) => `
+                    <div class="block">
+                        <div class="block-header">
+                            <h3>${intern.name}</h3>
+                            <h4>Intern</h4>
+                        </div>
+                        <div class="block-body">
+                            <p>ID: ${intern.id}</p>
+                            <p>Email: 
+                                <a href="mailto:${intern.email}">${intern.email}</a>
+                            </p>
+                            <p>School: ${intern.school}</p>
+                        </div>
+                    </div>`)}
+
+        </div>
+    </body>
+    </html>`; 
+}
+
+
 // ***********************************************************************************************
 
-const writeFile = (fileData) => {
-    fs.writeFile('./dist/index.html', fileData, err => {
+const writeFile = (data) => {
+    fs.writeFile('./dist/index.html', data, error => {
         if(error) {
             console.log(error);
             return;
@@ -212,18 +295,14 @@ const writeFile = (fileData) => {
 // ****************************************************************************************************
 
 
-function init() {
+
     addManager()
         .then(addEmployee)
-        .then((teamArray) => {
-            return generateHTML(teamArray);
+        .then(() => {
+            return indexPage()})
+        .then((data) => {
+          writeFile(data);
             })
-        .then((pageHTML) => {
-            return writeFile(pageHTML);
-            })
-        .catch((err) => {
-        console.log(err);
+        .catch((error) => {
+        console.log(error);
     });
-}
-
-init();
